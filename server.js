146181e1aa0240cd87f1ffb6e83a39c70ba898a9
Abu Taher <writerabutaher@gmail.com>
2023-07-20 19:43:17 +0600

    connect with DB

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/connectDB");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+connectDB();
 
 const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.json({ message: `server listening on port ${port}` });
 });
